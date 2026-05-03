0fc21a52a592f9328668e675bda4148193b1555d
Matheus <matheeuzinhoo282@gmail.com>
2026-05-03 20:41:11 -0300

    IMC

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿Console.WriteLine("Me informe seu peso");
+string pesoPessoa = Console.ReadLine()!;
+
+Console.WriteLine("Me informe sua altura");
+string alturaPessoa = Console.ReadLine()!;
+
+double altura = Convert.ToDouble(alturaPessoa);
+
+double peso = Convert.ToDouble(pesoPessoa); 
+
+int resultado = Convert.ToInt32(peso / (altura * altura));
+
+ Console.WriteLine($"este é seu IMC: {resultado} KG/m²");
